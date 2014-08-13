c71a00c32322e9bfc4680896aced41a381267b8c
ManReinsp <mani.reini@gmail.com>
2014-08-13 16:30:34 +0200

    Removed class files

--- LAPIS/src/com/icetee/lapis/main/Action.java
+++ LAPIS/src/com/icetee/lapis/main/Action.java
@@ -1,14 +1,18 @@
 package com.icetee.lapis.main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Action {
 	public String name;
 	private String execute;
 	private ArrayList<String> keywords;
+	ArrayList<ArrayList<String>> group; 
 	
 	public Action(JSONObject action) {
 		keywords = new ArrayList<String>();
@@ -18,11 +22,34 @@ public class Action {
 	private void parse(JSONObject action) {
 		name = action.getString("name");
 		execute = action.getString("execute");
+		
 		JSONArray keywordsjson = action.getJSONArray("keywords");
+		
 		for(int i = 0; i<keywordsjson.length(); i++) {
 			keywords.add(keywordsjson.getString(i));
 		}
-		
+
+		group = new ArrayList<ArrayList<String>>();
+		try {
+			JSONArray parametersjson = action.getJSONArray("parameters");
+			
+			for(int i = 0; i<parametersjson.length(); i++) {
+				ArrayList<String> temp = new ArrayList<String>();
+				JSONArray parameterjson = parametersjson.getJSONArray(i);
+				
+				for(int j = 0; j<parameterjson.length(); j++) {
+					temp.add(parameterjson.getString(j));
+				}
+				
+				group.add(temp);
+			}
+			
+			for(ArrayList<String> s : group) {
+				for(String t : s) {
+					System.out.println(t);
+				}
+			}
+		} catch(JSONException e) {}
 	}
 	
 	public void execute() {
