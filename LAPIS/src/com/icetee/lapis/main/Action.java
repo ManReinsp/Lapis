71e41f6033b12ffd2832c54a04bc79d7d224e933
ManReinsp <mani.reini@gmail.com>
2014-08-13 22:02:19 +0200

    Started documenting and further development

--- LAPIS/src/com/icetee/lapis/main/Action.java
+++ LAPIS/src/com/icetee/lapis/main/Action.java
@@ -9,27 +9,42 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Action {
+	// The name of the action in form PACKAGE - ACTION
 	public String name;
+	// A command line command that gets executed when this action is picked
 	private String execute;
+	// All keywords this action listens to
 	private ArrayList<String> keywords;
-	ArrayList<ArrayList<String>> group; 
+	// All appropriate sets of parameters to be used in this action
+	ArrayList<ArrayList<String>> parameters; 
 	
+	/**
+	 * Each Action gets initialized with its respective JSONObject that was taken from a <code>inf.json</code> file and holds all necessary information
+	 * 
+	 * @param action
+	 */
 	public Action(JSONObject action) {
-		keywords = new ArrayList<String>();
 		parse(action);
 	}
 	
 	private void parse(JSONObject action) {
+		// Initialize the keywords available for this Action 
+		keywords = new ArrayList<String>();
+
+		// Assign the name and execution code for this Action 
 		name = action.getString("name");
 		execute = action.getString("execute");
 		
+		// Pull off the keywords from the JSONObject and store them in a JSONArray
 		JSONArray keywordsjson = action.getJSONArray("keywords");
 		
+		// Loop over the keywords
 		for(int i = 0; i<keywordsjson.length(); i++) {
+			// Add each keyword into the ArrayList
 			keywords.add(keywordsjson.getString(i));
 		}
 
-		group = new ArrayList<ArrayList<String>>();
+		parameters = new ArrayList<ArrayList<String>>();
 		try {
 			JSONArray parametersjson = action.getJSONArray("parameters");
 			
@@ -41,27 +56,21 @@ public class Action {
 					temp.add(parameterjson.getString(j));
 				}
 				
-				group.add(temp);
-			}
-			
-			for(ArrayList<String> s : group) {
-				for(String t : s) {
-					System.out.println(t);
-				}
+				parameters.add(temp);
 			}
 		} catch(JSONException e) {}
 	}
 	
-	public void execute() {
+	public String execute() {
 		//TODO Implement
+		return execute + System.lineSeparator() + "It worked";
 	}
 	
 	public ArrayList<String> getKeywords() {
 		return keywords;
 	}
 	
-	public ArrayList<String>[] getParameter() {
-		//TODO Implement
-		return null;
+	public ArrayList<ArrayList<String>> getParameter() {
+		return parameters;
 	}
 }
--- LAPIS/src/com/icetee/lapis/main/ActionList.java
+++ LAPIS/src/com/icetee/lapis/main/ActionList.java
@@ -11,14 +11,58 @@ public class ActionList {
 	private HashMap<String, Action> keywords;
 	
 	public ActionList(ArrayList<String> files) {
+		loadActions(files);
+		gatherKeywords();
+	}
+	
+	/**
+	 * Load the given Strings as JSONObjects, split up the JSONArrays and create Actions 
+	 * @param files
+	 */
+	public void loadActions(ArrayList<String> files) {
+		// Initialize the actions Array
 		actions = new ArrayList<Action>();
 		
 		JSONArray json;
+		// Loop over all available strings
 		for(String file : files) {
+			// Parse those to JSONObjects and extract the JSONArray that holds Action informations
 			json = new JSONObject(file).getJSONArray("actions");
+			// Loop over the new JSONArray
 			for(int i = 0; i<json.length(); i++) {
+				// Create each respective Action by passing the JSONObject
 				actions.add(new Action(json.getJSONObject(i)));
 			}
 		}
+	}
+	
+	/**
+	 * Extract all Keywords from stored Actions and put them into a HashMap in form of (Keyword, Action) 
+	 */
+	public void gatherKeywords() {
+		// Initialize the keywords HashMap
+		keywords = new HashMap<String, Action>();
+		
+		// Loop over all existing Actions
+		for(Action a : actions) {
+			// Store the keywords for the current action
+			ArrayList<String> keywords = a.getKeywords();
+			// Loop over all Keywords
+			for(String k : keywords) {
+				System.out.println(k);
+				// Check if the keyword isn't already present
+				if(!this.keywords.containsKey(k)) {
+					// If it is new then enter it
+					this.keywords.put(k, a);
+				} else {
+					// If it is already in use then report collision
+					System.out.println("Keyword collision between " + this.keywords.get(k).name + " and " + a.name + "!");
+				}
+			}
+		}
+	}
+	
+	public HashMap<String, Action> getKeywords() {
+		return keywords;
 	}
 }
